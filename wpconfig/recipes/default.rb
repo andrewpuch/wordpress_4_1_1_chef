576536fe203b8b9a797b4a507f4f7921edbdc142
Andy Puch <andrewpuch@gmail.com>
2015-02-18 20:03:55 -0500

    Changing owners and group

--- wpconfig/recipes/default.rb
+++ wpconfig/recipes/default.rb
@@ -2,6 +2,6 @@ cookbook_file "/srv/www/wordpress/current/wp-config.php" do
   source "wp-config.php"
   mode '0755'
   action :create
-  owner 'apache'
-  group 'apache'
+  owner 'deploy'
+  group 'www-data'
 end
