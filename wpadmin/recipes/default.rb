684e3cc8b53f476574da844e4afe051725bd3ab3
Andy Puch <apuch@corp.paltalk.com>
2015-04-01 14:44:36 -0400

    Quick updates for chef recipes.

---
+++ wpadmin/recipes/default.rb
@@ -0,0 +1,4 @@
+directory '/srv/www/wordpress/current/wp-admin/' do
+    recursive true
+    action :delete
+end
--- wplogin/recipes/default.rb
+++ wplogin/recipes/default.rb
@@ -1,3 +1,3 @@
-cookbook_file '/srv/www/wordpress/current/wp-login.php' do
+file '/srv/www/wordpress/current/wp-login.php' do
     action :delete
 end
